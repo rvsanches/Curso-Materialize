6ea681c2a3cfc63d74d5e5ec90e81861de497c34
Ricardo Sanches <ricardo.sanches@live.com>
2019-03-10 11:35:55 -0400

    Arquivo com botões

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Floating Action Button
+const elemsBtns = document.querySelectorAll('.fixed-action-btn');
+const floatingButton = M.FloatingActionButton.init(elemsBtns, {
+    direction: 'left',
+    hoverEnabled: false
+});
