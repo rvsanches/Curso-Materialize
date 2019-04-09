6db7eaa16c9315c3119ea2eef77a697d22db483b
Ricardo Sanches <ricardo.sanches@live.com>
2019-04-08 23:00:58 -0400

    Adição do Navbar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 // Floating Action Button
-const emensBtns = document.querySelectorAll(".fixed-action-btn");
-const floatingBtn = M.FloatingActionButton.init(emensBtns, {
+const elemsBtns = document.querySelectorAll(".fixed-action-btn");
+const floatingBtn = M.FloatingActionButton.init(elemsBtns, {
     direction: "left",
     hoverEnabled: false
+});
+
+// Navbar
+const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
+const instancesDropdown = M.Dropdown.init(elemsDropdown, {
+    coverTrigger: false
+});
+elemsSidenav = document.querySelectorAll(".sidenav");
+const instancesSidenav = M.Sidenav.init(elemsSidenav, {
+    edge: "left"
 });
